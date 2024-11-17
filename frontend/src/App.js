22513847105732627d3e4415da42fb9a683b9a8c
Alex Szanti <alexszanti@gmail.com>
2024-11-16 22:30:57 -0500

    Added Semester Select Screen
    Screen for user to select semester. Currently any of the three options route to the same app.js page, but for now this is okay. Styling and proper routing can be later worked on.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,39 +3,50 @@ import React, { useEffect, useState } from 'react';
 import PlanList from './PlanList';
 import Navbar from './Navbar';
 import Calendar from './Calendar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SemSelect from './semSelect'; 
 
 const App = () => {
   const [plans, setPlans] = useState([]);
   const [selectedPlanId, setSelectedPlanId] = useState(null);
   const [selectedPlan, setSelectedPlan] = useState(null);
 
-  useEffect(() => { setSelectedPlan(plans.find(plan => plan.id === selectedPlanId)); }, [selectedPlanId, plans]);
+  useEffect(() => { setSelectedPlan(plans.find((plan) => plan.id === selectedPlanId));
+  }, [selectedPlanId, plans]);
 
   const addNewPlan = () => {
-    const newPlan = {id: Date.now(), name: `Plan ${plans.length + 1}`, events: [] };
+    const newPlan = { id: Date.now(), name: `Plan ${plans.length + 1}`, events: [] };
     setPlans([...plans, newPlan]);
-  }
+  };
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <div className="sidebar">
-          <PlanList
-            plans={plans}
-            onSelectPlan={setSelectedPlanId}
-            onAddPlan={addNewPlan}
-            selectedPlanId={selectedPlanId}
-          />
-        </div>
-        <div className="calendar-container">
-           {selectedPlan && (
-          <Calendar plan={selectedPlan} />
-      )}
-      </div>
-    </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SemSelect />} />
+        <Route
+          path="/plans"
+          element={
+            <div className="App">
+              <Navbar />
+              <div className="content">
+                <div className="sidebar">
+                  <PlanList
+                    plans={plans}
+                    onSelectPlan={setSelectedPlanId}
+                    onAddPlan={addNewPlan}
+                    selectedPlanId={selectedPlanId}
+                  />
+                </div>
+                <div className="calendar-container">
+                  {selectedPlan && <Calendar plan={selectedPlan} />}
+                </div>
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
